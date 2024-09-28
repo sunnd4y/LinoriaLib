d66c57ff862781edfdd287fc26985399bbd60ace
sunnd4y <120350465+sunnd4y@users.noreply.github.com>
2024-09-28 12:53:08 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2994,6 +2994,7 @@ function Library:CreateWindow(...)
         TextXAlignment = Enum.TextXAlignment.Left;
         ZIndex = 1;
         Parent = Inner;
+        RichText = true;
     });
 
     local MainSectionOuter = Library:Create('Frame', {
